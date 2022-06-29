06e81e7894fd26c0aadce7f123ebc31874074508
JustCarmen <carmen@justcarmen.nl>
2022-06-29 15:41:59 +0200

    Fix #1 - do not use a hard coded folder name.

---
+++ MediaDisplayModule.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\MediaDisplay;
+
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\View;
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Fisharebest\Webtrees\Module\ModuleCustomTrait;
+use Fisharebest\Webtrees\Module\ModuleCustomInterface;
+
+/**
+ * Display media objects as in webtrees 2.0
+ */
+class MediaDisplayModule extends AbstractModule implements ModuleCustomInterface
+{
+    use ModuleCustomTrait;
+
+     /**
+     * @var string
+     */
+    public const CUSTOM_AUTHOR = 'JustCarmen';
+
+    /**
+     * @var string
+     */
+    public const CUSTOM_VERSION = '1.2';
+     /**
+     * @var string
+     */
+    public const GITHUB_REPO = 'webtrees-media-display';
+
+     /**
+     * @var string
+     */
+    public const AUTHOR_WEBSITE = 'https://justcarmen.nl';
+
+    /**
+     * How should this module be identified in the control panel, etc.?
+     *
+     * @return string
+     */
+    public function title(): string
+    {
+
+        /* I18N: Name of a module */
+        return I18N::translate('Media Display (functionallity as in webtrees 2.0.x versions)');
+    }
+
+    /**
+     * A sentence describing what this module does.
+     *
+     * @return string
+     */
+    public function description(): string
+    {
+        /* I18N: Description of the “Simple Menu” module */
+        return I18N::translate('Display media objects as in webtrees 2.0, showing multiple media items and additional notes belonging to the same object on the individual page.');
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleAuthorName()
+     */
+    public function customModuleAuthorName(): string
+    {
+        return self::CUSTOM_AUTHOR;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleVersion()
+     */
+    public function customModuleVersion(): string
+    {
+        return self::CUSTOM_VERSION;
+    }
+
+    /**
+     * A URL that will provide the latest stable version of this module.
+     *
+     * @return string
+     */
+    public function customModuleLatestVersionUrl(): string
+    {
+        return 'https://raw.githubusercontent.com/' . self::CUSTOM_AUTHOR . '/' . self::GITHUB_REPO . '/main/latest-version.txt';
+    }
+
+    /**
+     * Fetch the latest version of this module.
+     *
+     * @return string
+     */
+    public function customModuleLatestVersion(): string
+    {
+        return 'https://github.com/' . self::CUSTOM_AUTHOR . '/' . self::GITHUB_REPO . '/releases/latest';
+    }
+
+     /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleSupportUrl()
+     */
+    public function customModuleSupportUrl(): string
+    {
+        return self::AUTHOR_WEBSITE;
+    }
+
+    /**
+     * Bootstrap the module
+     */
+    public function boot(): void
+    {
+        // Register a namespace for our views.
+        View::registerNamespace($this->name(), $this->resourcesFolder() . 'views/');
+
+        // Replace an existing view with our own version.
+        View::registerCustomView('::fact-gedcom-fields', $this->name() . '::fact-gedcom-fields');
+    }
+
+     /**
+     * Where does this module store its resources
+     *
+     * @return string
+     */
+    public function resourcesFolder(): string
+    {
+        return __DIR__ . '/resources/';
+    }
+};
